a6e1655508ffc537bf067b4b0f1af51c4eec0225
josephaleps1 <lepyraljosepha@gmail.com>
2016-04-10 18:50:09 +0200

    Update mon.py

--- mon.py
+++ mon.py
@@ -1,2 +1,9 @@
 print "test"
+from math import random
+def grille():
+  input("saisissez une grille:")
+  input("saisissez le taux d'occupation:")
+  N=input("saisissez une grille:")
+  for i in range(N):
+    print("N*N)
 
